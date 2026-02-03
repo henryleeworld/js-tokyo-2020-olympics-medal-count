f3348859f5c9ce89ee580614d6bee3a9ab3f35b9
Henry Lee <henryleeworld@gmail.com>
2026-02-04 00:38:19 +0800

    feat:更新最新資料

--- js/olympics-medal-count.js
+++ js/olympics-medal-count.js
@@ -1,43 +1,45 @@
 $(function() {
-    var country;
-    $.getJSON("data/countries.json", function(json) {
-        country = json;
-    });
-    $('#medal-count-table').DataTable({
-        ajax: {
-            url: "https://olympics.news-engineering.aws.wapo.pub/2020/medals/totals.json",
-            dataSrc: ""
-        },
-        columns: [{
-                "data": "rank"
+    $.getJSON('data/countries.json', function(country) {
+        $('#medal-count-table').DataTable({
+            ajax: {
+                url: 'https://olympics.news-engineering.aws.wapo.pub/2020/medals/totals.json',
+                dataSrc: ''
             },
-            {
-                "data": "title"
+            columns: [{
+                    data: 'rank'
+                },
+                {
+                    data: 'title'
+                },
+                {
+                    data: 'gold'
+                },
+                {
+                    data: 'silver'
+                },
+                {
+                    data: 'bronze'
+                },
+                {
+                    data: 'total'
+                }
+            ],
+            columnDefs: [{
+                targets: 1,
+                render: function(data, type, row) {
+                    return `
+                        <img src="${row.icon}" width="20" height="20">
+                        ${country[row.id] ?? ''}
+                    `;
+                }
+            }],
+            language: {
+                url: 'https://cdn.datatables.net/plug-ins/2.3.7/i18n/zh-HANT.json'
             },
-            {
-                "data": "gold"
-            },
-            {
-                "data": "silver"
-            },
-            {
-                "data": "bronze"
-            },
-            {
-                "data": "total"
-            },
-        ],
-        columnDefs: [{
-            targets: 1,
-            data: 'title',
-            render: function(data, type, row, meta) {
-                return '<img src="' + row.icon + '" class="" width="20" height="20"/> ' + country[row.id];
-            }
-        }],
-        language: {
-            url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/zh-HANT.json',
-        },
-        order: [[5, 'desc']],
-        responsive: true
+            order: [
+                [5, 'desc']
+            ],
+            responsive: true
+        });
     });
 });
